91dcb8ec38d563b7ed3a55720d620756b4ae49f2
Tim <github@timgreen.ws>
2020-08-10 15:15:30 +1000

    added html template structure for a section

---
+++ inc/section-template.php
@@ -0,0 +1,15 @@
+<!-- section start-->
+ <a href="" target="_blank">
+    <section>
+        <div class="section_title">title</div>
+           <div class="about_section">
+               <span>title</span>
+            </div>
+            
+            <div class="bottom_section">
+                <span><i class="fal fa-globe"></i> Title</span>
+                <span><i class="fal fa-folders"></i> Title</span>
+             </div>
+    </section>
+</a>
+<!-- section ends-->
