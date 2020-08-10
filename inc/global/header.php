a0d47c80e2959cb9f178a1ab4cb1dd4e1cc89219
Tim <github@timgreen.ws>
2020-08-10 15:13:46 +1000

    created header that includes site title, icon and navigation to access demos

---
+++ inc/global/header.php
@@ -0,0 +1,15 @@
+<div class="logo site-sidebar">
+    <div class="logo-icon">
+        <i class="far fa-compass mapbox-icon"></i>
+        <h1 class="site-name">Mapbox</h1>
+    </div>
+    <div class="logo-title">
+       <!-- Mapbox Demo's --> Demo's Below
+    </div>
+
+    <div class="navigation">
+        <?php include('inc/navigation.php');?>
+    </div>
+
+    </div>
+</div>
