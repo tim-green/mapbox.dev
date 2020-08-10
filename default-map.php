00397f5cd11ef3d00bbd2ce36c9ba076b6133019
Tim <github@timgreen.ws>
2020-08-10 18:11:31 +1000

    added code preview of default map

--- default-map.php
+++ default-map.php
@@ -59,16 +59,15 @@
 
 
 <pre>
-  <code>
-  <script>
+  <code class="language-js">
             mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JvdW5kY3RybCIsImEiOiJjanhvb2FuczkwOTBxM2RwOWR2M2dzcTBvIn0.4OIjhU9J4sQVJGkNIF1eVg';
             var map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11'
             });
-            </script>
   </code>
 </pre>
+
     <!-- section ends-->
     
 
