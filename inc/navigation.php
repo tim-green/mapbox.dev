59981e0a9473e46496bd27f914ee85210ebcdf72
Tim <github@timgreen.ws>
2020-08-10 19:22:57 +1000

    added future maps for future development

--- inc/navigation.php
+++ inc/navigation.php
@@ -7,11 +7,55 @@
 <!-- home to display on sidebar only-->
 
 <!-- section start-->
-<a href="default-map.php">
+<a href="map-default.php">
     <section class="map-item">
         <div class="section_title">Default Mapbox</div>
            <div class="about_section">
                <span>Default view of a map using mapbox</span>
+            </div>
+    </section>
+</a>
+<!-- section ends-->
+
+<!-- section start-->
+<a href="map-driving-directions.php">
+    <section class="map-item">
+        <div class="section_title">Mapbox Driving Directions</div>
+           <div class="about_section">
+               <span>Using Mapbox to show driving directions</span>
+            </div>
+    </section>
+</a>
+<!-- section ends-->
+<!-- section start-->
+<a href="map-local-search.php">
+    <section class="map-item">
+        <div class="section_title">Future Map: Map with Local Search</div>
+           <div class="about_section">
+               <span>Local search with the Geocoding API</span>
+            </div>
+    </section>
+</a>
+<!-- section ends-->
+
+<!-- section start-->
+<a href="#">
+    <section class="map-item">
+        <div class="section_title">Future Map: Store Locator</div>
+           <div class="about_section">
+               <span>Store locator</span>
+            </div>
+    </section>
+</a>
+<!-- section ends-->
+
+
+<!-- section start-->
+<a href="#">
+    <section class="map-item">
+        <div class="section_title">Future Map: Optimised route</div>
+           <div class="about_section">
+               <span>Generating an optimised route with the Optimization API</span>
             </div>
     </section>
 </a>
