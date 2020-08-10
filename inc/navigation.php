baf3490f7106fdf1bc06bed14bb93a4bb0807d23
Tim <github@timgreen.ws>
2020-08-10 15:16:04 +1000

    navigation of mapbox demos

---
+++ inc/navigation.php
@@ -0,0 +1,18 @@
+<!-- home to display on sidebar only-->
+<a href="index.php" class="home-link">
+    <section>
+        <div class="section_title">Home</div>
+    </section>
+</a>
+<!-- home to display on sidebar only-->
+
+<!-- section start-->
+<a href="default-map.php">
+    <section class="map-item">
+        <div class="section_title">Default Mapbox</div>
+           <div class="about_section">
+               <span>Default view of a map using mapbox</span>
+            </div>
+    </section>
+</a>
+<!-- section ends-->
