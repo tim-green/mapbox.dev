d6d496ffe0c27a02d24c3be12364e1e9b338cf08
Tim <github@timgreen.ws>
2020-08-16 21:33:21 +1000

    added prism scripts ina php file for includes

---
+++ inc/global/prism-load.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.21.0/prism.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.21.0/components/prism-core.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.21.0/plugins/autoloader/prism-autoloader.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.21.0/plugins/show-language/prism-show-language.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/prismjs@1.21.0/plugins/keep-markup/prism-keep-markup.min.js"></script>
+
+
+<script src="https://cdn.jsdelivr.net/combine/npm/jquery@3,npm/jquery-match-height@0.7.2,npm/prismjs@1,npm/prismjs@1/components/prism-core.min.js,npm/prismjs@1/plugins/autoloader/prism-autoloader.min.js,npm/prismjs@1/plugins/show-language/prism-show-language.min.js,npm/prismjs@1/plugins/keep-markup/prism-keep-markup.min.js"></script>
